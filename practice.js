62a64587e43c969e5e8182dc2362d760f095a368
Andy Puertas <andy.puertas08@gmail.com>
2018-09-07 11:36:18 -0600

    finished

--- practice.js
+++ practice.js
@@ -103,7 +103,7 @@ var looper = (array) => {
   var mySum = 0;
   for(var i = 0; i < array.length; i++) {
     if(array[i] % 2 !== 0 || array[i] >= 100) {
-      array[i] += mySum;
+     mySum += array[i]
     }
   }
   return mySum;
@@ -121,12 +121,14 @@ function math(num1, num2, callback) {
 //Write a function called add that takes in two parameters and
 //returns the result of adding them together.
 
-//Code Here
+var add = (num1, num2) => {
+  return num1 += num2;
+}
 
 //Now invoke math, passing in the numbers 3 and 4, and your add function,
 //storing the result in the variable mathSum.
 
-//Code Here
+var mathSum = math(3, 4, add);
 
 //////////////////PROBLEM 8////////////////////
 
@@ -141,7 +143,9 @@ function sampleCallbackTwo() {
   return "I am also a callback function";
 }
 
-//Code Here
+var invoker = (sampleCallbackOne) => {
+  return sampleCallbackOne()
+}
 
 //////////////////PROBLEM 9////////////////////
 
@@ -165,28 +169,32 @@ function pond() {
 //as strings.
 
 //This array should contain the variable names (as strings) accessible in the global scope.
-let globalScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let globalScope = ["duck", "rubberDuck", "realDuck"];
 
 //This array should contain the variable names (as strings) accessible in the bathroom function.
-let bathroomScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathroomScope = ["duck", "sailorDuck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the bathtub function.
-let bathtubScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let bathtubScope = ["duck", "sailorDuck", "rubberDuck"];
 
 //This array should contain the variable names (as strings) accessible in the pond function.
-let pondScope = ["duck", "sailorDuck", "rubberDuck", "realDuck"];
+let pondScope = ["duck", "realDuck"];
 
 //////////////////PROBLEM 10////////////////////
 
 //Create a function called outerFn which returns an anonymous
 //function which returns your name.
 
-//Code Here
+function outerFn() {
+  return function anon() {
+    return "Andy"
+  }
+}
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
-//Code Here
+var innerFn = outerFn();
 
 //Now invoke innerFn and save the result to a variable called finalResult.
 
-//Code Here
+let finalResult = innerFn();
